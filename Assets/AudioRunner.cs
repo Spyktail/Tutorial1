9aa49852622b9576c2027f16208b3d0edf0c7d26
Spyktail <rad326@knights.ucf.edu>
2021-02-18 19:20:25 -0500

    First commit

---
+++ Assets/AudioRunner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRunner : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClip;
+    public AudioClip soundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClip;
+            musicSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = soundEffect;
+            musicSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+        }
+    }
+}
